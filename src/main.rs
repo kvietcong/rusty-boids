d67fa49f89c24e261f112937672c0ce29dc1ed3f
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-03-12 16:47:22 -0800

    🖱️ Add Ability to Retrieve Cursor State

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    input::{mouse::MouseButtonInput, ElementState},
     prelude::*,
 };
 
@@ -15,11 +16,22 @@ pub enum DebugState {
     Off,
 }
 
+#[derive(Default)]
+pub struct Cursor {
+    pub position: Vec2,
+    pub button_states: [bool; 3],
+}
+
+#[derive(Component)]
+struct MainCamera;
+
 const WIDTH: f32 = 1600.0;
 const HEIGHT: f32 = 900.0;
 
 fn setup_cameras(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn_bundle(OrthographicCameraBundle::new_2d())
+        .insert(MainCamera);
     commands.spawn_bundle(UiCameraBundle::default());
 }
 
@@ -29,10 +41,7 @@ fn setup_window(mut windows: ResMut<Windows>) {
     window.set_title("Le Boids".to_string());
 }
 
-fn handle_input_presses_system(
-    keys: Res<Input<KeyCode>>,
-    mut debug_state: ResMut<State<DebugState>>,
-) {
+fn handle_key_system(keys: Res<Input<KeyCode>>, mut debug_state: ResMut<State<DebugState>>) {
     if keys.just_pressed(KeyCode::D) && keys.just_pressed(KeyCode::LShift) {
         let current_sim_state = debug_state.current();
         let new_debug_state = match current_sim_state {
@@ -40,6 +49,37 @@ fn handle_input_presses_system(
             DebugState::Off => DebugState::On,
         };
         debug_state.set(new_debug_state).unwrap();
+    }
+}
+
+fn cursor_system(
+    windows: Res<Windows>,
+    mut cursor: ResMut<Cursor>,
+    mut mouse_button_events: EventReader<MouseButtonInput>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    let (camera, camera_transform) = camera_query.single();
+    let window = windows.get(camera.window).unwrap();
+    if let Some(screen_pos) = window.cursor_position() {
+        let window_size = Vec2::new(window.width(), window.height());
+
+        let normalized_device_coordinates = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+        let normalized_device_coordinates_to_world =
+            camera_transform.compute_matrix() * camera.projection_matrix.inverse();
+        let world_pos = normalized_device_coordinates_to_world
+            .project_point3(normalized_device_coordinates.extend(-1.0));
+        let world_pos: Vec2 = world_pos.truncate();
+
+        cursor.position = world_pos;
+    }
+    for event in mouse_button_events.iter() {
+        let button_index = match event.button {
+            MouseButton::Left => 0,
+            MouseButton::Middle => 1,
+            MouseButton::Right => 2,
+            _ => continue,
+        };
+        cursor.button_states[button_index] = event.state == ElementState::Pressed;
     }
 }
 
@@ -51,6 +91,7 @@ fn main() {
         .add_startup_system(setup_window) // IDK Why the window doesn't resize with the descriptor
         .add_startup_system(setup_cameras)
         .add_state(DebugState::Off)
+        .insert_resource(Cursor::default())
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(WindowDescriptor {
             title: "Bevy - Le Boids".to_string(),
@@ -61,7 +102,7 @@ fn main() {
             ..WindowDescriptor::default()
         }); // For some reason, this doesn't do anything
 
-    app.add_system(handle_input_presses_system);
+    app.add_system(handle_key_system).add_system(cursor_system);
 
     // Adding Boids Simulation
     app.add_plugin(BoidsPlugin::default());
@@ -78,8 +119,3 @@ fn main() {
 
     app.run();
 }
-
-// TODO:
-// - Make live-adjustable factors
-// - Make flocking more streamlined
-// - Have flocking groups
